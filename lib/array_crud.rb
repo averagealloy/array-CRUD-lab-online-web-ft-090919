60246f809e07e61f5cfa0d2fd31f67e95848e42f
Mike McTighe <m.mctighe10@gmail.com>
2019-09-10 19:10:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  color_array = ["red","green","blue","yellow","pur"]
+  color_array = ["red","green","blue","yellow","purple"]
   color_array.unshift("rainbow")
 end
 
