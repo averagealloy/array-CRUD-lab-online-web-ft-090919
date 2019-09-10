ad9b9c5c42477178854bbe28e322e1d2580ead96
Mike McTighe <m.mctighe10@gmail.com>
2019-09-10 19:09:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  color_array = ["red","green","blue","yellow","arrays!"]
+  color_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
