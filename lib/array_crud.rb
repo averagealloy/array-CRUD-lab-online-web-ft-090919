cd5143ba67480acfe61371290df21828cbeb49f7
Mike McTighe <m.mctighe10@gmail.com>
2019-09-10 19:15:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,8 +18,8 @@ end
 
 
 def remove_element_from_end_of_array(array)
-  color_array = ["rainbow","red","green","blue","yellow","purple"]
-  color_array.pop
+color_array = ["wow","red","green","blue","yellow","arrays!"]
+color_array.pop
 end
 
 def remove_element_from_start_of_array(array)
